601df08ec135bd54fc58a46bec2f27f6e371727b
Marco Heyden <heyden@ipe-nilpferd.local>
2020-03-31 00:50:20 +0200

    Added sliding window for measuring switching cost

--- src/main/scala/utils/MeasuresSwitchingTime.scala
+++ src/main/scala/utils/MeasuresSwitchingTime.scala
@@ -1,5 +1,6 @@
 package utils
 
+import scala.collection.mutable.Queue
 import io.github.edouardfouche.utils.StopWatch
 
 class MeasuresSwitchingTime {
@@ -9,11 +10,19 @@ class MeasuresSwitchingTime {
 	private var end_time = 0.0		// the time the round ended
 	private var t_cs_prev = 0.0		// the switching time of the previous round
 	private var t_1_prev = 0.0		// the iterating time of the previous round
-	private var counter = 0			// the counter how often we measured
+
+	val queue_tcs = new Queue[Double]
+	val queue_t1 = new Queue[Double]
+
+	def enqueue(q: Queue[Double], new_val: Double, max_size: Int = 10): Unit = {
+		q.enqueue(new_val)
+		if (q.size > max_size) {
+			q.dequeue
+		}
+	}
 
 	def init_execution(): Unit = {
 		t_m_total = 0.0
-		counter = 0
 		t_cs_prev = 0.0
 		t_1_prev = 0.0
 		start_time = 0.0
@@ -35,12 +44,12 @@ class MeasuresSwitchingTime {
 	def calculate_switching_time(N: Int, m_round: Int): (Double, Double) = {
 		end_time = StopWatch.stop()._1
 		val duration = end_time - start_time // contains switching time, wait time, processing
-		val m = m_round.toDouble / N
 		val t_cs_new = ( duration - t_m_total ) / N
 		val t_1_new = t_m_total / m_round
-		val t_cs = (counter * t_cs_prev + t_cs_new) / (counter+1)
-		val t_1 = (counter * t_1_prev + t_1_new) / (counter+1)
-		counter += counter
+		enqueue(queue_tcs, t_cs_new)
+		enqueue(queue_t1, t_1_new)
+		val t_cs = queue_tcs.sum / queue_tcs.size
+		val t_1 = queue_t1.sum / queue_t1.size
 		val measurement = (t_cs, t_1)
 		measurement
 	}
